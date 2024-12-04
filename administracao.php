c6daea68f1ce1f7948756e3e3a746185aee157b5
DanielNerisAntunes <danielnantunestecnicoetb@gmail.com>
2024-12-04 10:36:04 -0300

    Atualizar o administracao.php
    Troquei o nome do site para multi motos

--- administracao.php
+++ administracao.php
@@ -1,40 +1,43 @@
 <?php
-	session_start();
+    session_start();
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: login.php");
+        exit();
+    }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Administração</title>
+    <title>Administração - Multi Motos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/layout.css">
     <link rel="stylesheet" type="text/css" href="css/menu.css">
     <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
+    <meta name="description" content="Sistema de administração da Multi Motos. Gerencie produtos e clientes.">
 </head>
 <body>
     <div id="principal">
         <div id="topo">
             <div id="logo">
-                <h1> LOJA DE MOTOS </h1>
-                <h1> Nome da Loja </h1>
+                <h1> MULTI MOTOS </h1>
+                <h1> Multi Motos </h1>
             </div>
-            <div id="menu_global"  class="menu_global">
-                <p align="right"> Olá <?php include "valida_login.php"; ?> </p>
+            <div id="menu_global" class="menu_global">
+                <p align="right"> Olá, <?php echo htmlspecialchars($_SESSION['usuario'], ENT_QUOTES, 'UTF-8'); ?> </p>
                 <?php include "menu_local.php"; ?>               
             </div>
         </div>
         <div id="conteudo_especifico">
             <h1> ADMINISTRAÇÃO </h1>
-            <p>Seja bem-vindo ao sistema de gerenciamento da Loja de Motos Nome da Loja</p>
+            <p>Seja bem-vindo ao sistema de gerenciamento da Multi Motos</p>
         </div>
         <div id="rodape">
             <div id="texto_institucional">
-                <div id="texto_institucional">
-                    <h6> CONTROLE DE MOTOS </h6> 
-                    <h6> Rua das Motos, 123 -- E-mail: contato@lojademotos.com.br -- Fone: (11) 9999-9999 </h6> 
-                </div> 
-            </div>				
+                <h6> CONTROLE DE MOTOS </h6> 
+                <h6> Rua das Motos, 123 -- E-mail: contato@multimotos.com.br -- Fone: (11) 9999-9999 </h6> 
+            </div> 
         </div>
     </div>
 </body>
