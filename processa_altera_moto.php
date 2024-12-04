ad0756b1f01424f0ab91ed833aab3186a705e974
DanielNerisAntunes <danielnantunestecnicoetb@gmail.com>
2024-12-04 11:44:21 -0300

    Atualizar o processa_altera_moto.php
    alterações cod

--- processa_altera_moto.php
+++ processa_altera_moto.php
@@ -1,55 +1,42 @@
-<?php	
-$conectar = mysqli_connect ("localhost", "root", "", "motos");				
+<?php
+$conectar = mysqli_connect("localhost", "root", "", "motos");
 
 $cod = $_POST["codigo"];
-$marca = $_POST["marca"];	
+$marca = $_POST["marca"];
 $modelo = $_POST["modelo"];
 $ano = $_POST["ano"];
 $preco = $_POST["preco"];
 $tipo = $_POST["tipo"];
 $foto = $_FILES["foto"];
 
-
-if ($foto["name"] <> "") {
-    $foto_nome = "img/".$foto["name"];		
+// Verificar se uma nova foto foi enviada
+if ($foto["name"] != "") {
+    $foto_nome = "img/" . $foto["name"];
     move_uploaded_file($foto["tmp_name"], $foto_nome);
-}
-else {
-    $pesquisa_caminho_foto = "SELECT foto_moto
-                             FROM motos
-                             WHERE Cod_moto = '$cod'";
-    $resultado_pesquisa = mysqli_query ($conectar, $pesquisa_caminho_foto);
-    $registro = mysqli_fetch_row ($resultado_pesquisa);
-    $foto_nome = $registro[0];
+} else {
+    // Se não foi enviada uma foto, obter a foto atual do banco de dados
+    $pesquisa_caminho_foto = "SELECT foto_moto FROM motos WHERE Cod_moto = '$cod'";
+    $resultado_pesquisa = mysqli_query($conectar, $pesquisa_caminho_foto);
+    $registro = mysqli_fetch_row($resultado_pesquisa);
+    $foto_nome = $registro[0]; // Mantém o caminho da foto anterior
 }
 
-$sql_altera = "UPDATE motos 		
-              SET 		Marca='$marca', 
-                        Modelo = '$modelo',
-                        Ano_de_fabricacao ='$ano', 
-                        Preco_de_venda ='$preco', 
-                        foto_moto = '$foto_nome'
-              WHERE 	Cod_moto = '$cod'";
-$sql_resultado_alteracao = mysqli_query ($conectar, $sql_altera);
+// Usar prepared statements para evitar SQL injection
+$sql_altera = "UPDATE motos SET Marca = ?, Modelo = ?, Ano_de_fabricacao = ?, Preco_de_venda = ?, foto_moto = ? WHERE Cod_moto = ?";
+$stmt = mysqli_prepare($conectar, $sql_altera);
+mysqli_stmt_bind_param($stmt, "sssssi", $marca, $modelo, $ano, $preco, $foto_nome, $cod);
+$sql_resultado_alteracao = mysqli_stmt_execute($stmt);
 
-if ($sql_resultado_alteracao == true)
-{
+if ($sql_resultado_alteracao) {
     echo "<script>
-            alert ('$modelo alterado com sucesso') 
-          </script>";
-    echo "<script> 
-             location.href = ('lista_motos.php') 
+            alert('$modelo alterado com sucesso');
+            location.href = 'lista_motos.php';
           </script>";
     exit();
-}  
-else
-{    
-    echo "<script> 
-            alert ('Ocorreu um erro no servidor. 
-                    Dados da moto não foram alterados. Tente de novo') 
-        </script>";
-    echo "<script> 
-            location.href ('altera_moto.php?codig=$cod') 
-         </script>";
+} else {
+    echo "<script>
+            alert('Ocorreu um erro no servidor. Dados da moto não foram alterados. Tente de novo');
+            location.href = 'altera_moto.php?codigo=$cod';
+          </script>";
 }
 ?>
