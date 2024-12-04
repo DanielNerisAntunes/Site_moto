c57423cf65e5c16aa5bc541cab5123c07a130900
DanielNerisAntunes <danielnantunestecnicoetb@gmail.com>
2024-12-04 11:38:01 -0300

    Atualizar o menu_local.php
    adição de fila compra

--- menu_local.php
+++ menu_local.php
@@ -1,30 +1,37 @@
 <?php
-if ($_SESSION["funcao_fun"] == "administrador") {
+$conectar = mysqli_connect("localhost", "root", "", "motos");
+
+$sql_estoque = "SELECT Cod_moto, Marca, Modelo, Preco_de_venda, Saldo 
+                FROM motos 
+                WHERE Saldo > 0";  // Filtrando produtos com saldo maior que 0
+
+$resultado_estoque = mysqli_query($conectar, $sql_estoque);
 ?>
-<ul>
-    <li><a href="administracao.php" class="active">Administração</a></li>
-    <li><a href="lista_fun.php" class="active">Funcionários</a></li>
-    <li><a href="lista_motos.php">Motos</a></li>
-    <li><a href="vendas.php">Vendas</a></li>     
-    <li><a href="relatorios.php">Relatorios</a></li>
-    <li><a href="logout.php" class="active">Sair</a></li>  
-</ul> 
-<?php
-} elseif ($_SESSION["funcao_fun"] == "estoquista") {
-?>
-<ul>
-    <li><a href="administracao.php" class="active">Administração</a></li>
-    <li><a href="lista_motos.php">Motos</a></li>  
-    <li><a href="logout.php" class="active">Sair</a></li>
-</ul> 
-<?php		
-} else {
-?>
-<ul>
-    <li><a href="administracao.php" class="active">Administração</a></li>
-    <li><a href="vendas.php">Vendas</a></li>    
-    <li><a href="logout.php" class="active">Sair</a></li>
-</ul> 
-<?php		
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Relatório de Estoque</title>
+</head>
+<body>
+    <h1>Relatório de Estoque (Somente Produtos com Saldo)</h1>
+    <table border="1">
+        <tr>
+            <th>Código</th>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Preço</th>
+            <th>Saldo</th>
+        </tr>
+        <?php while ($registro = mysqli_fetch_row($resultado_estoque)) { ?>
+        <tr>
+            <td><?php echo $registro[0]; ?></td>
+            <td><?php echo $registro[1]; ?></td>
+            <td><?php echo $registro[2]; ?></td>
+            <td><?php echo $registro[3]; ?></td>
+            <td><?php echo $registro[4]; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
