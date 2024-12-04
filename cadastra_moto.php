936f09b2a4348a320bab3edbf64b67d6c1366e23
DanielNerisAntunes <danielnantunestecnicoetb@gmail.com>
2024-12-04 11:09:45 -0300

    Atualizar o cadastra_moto.php
    adicionei select

--- cadastra_moto.php
+++ cadastra_moto.php
@@ -2,11 +2,11 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cadastro de Motos</title>
+    <title>Cadastro de Motos - Multi Motos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/layout.css">
     <link rel="stylesheet" type="text/css" href="css/menu.css">
@@ -16,11 +16,11 @@ session_start();
     <div id="principal">
         <div id="topo">
             <div id="logo">
-                <h1> LOJA DE MOTOS </h1>
-                <h1> Nome da Loja </h1>
+                <h1> MULTI MOTOS </h1>
+                <h1> Multi Motos </h1>
             </div>
-            <div id="menu_global"  class="menu_global">
-                <p align="right"> Olá <?php include "valida_login.php";?> </p>
+            <div id="menu_global" class="menu_global">
+                <p align="right"> Olá <?php include "valida_login.php"; ?> </p>
                 <?php include "menu_local.php"; ?>               
             </div>
         </div>
@@ -34,29 +34,55 @@ session_start();
                 <p> Cor: <input type="text" name="cor" required> </p>
                 <p> Chassi: <input type="text" name="chassi" required> </p>
                 <p> Cilindrada: <input type="text" name="cilindrada" required> </p>
-                <p> Tipo: <input type="text" name="tipo" required> </p>
-                <p> Preço de custo: <input type="number" name="preco_custo" required> </p>
-                <p> Preço de venda: <input type="number" name="preco_venda" required> </p>
+                <p> Tipo: 
+                    <select name="tipo" required>
+                        <option value="esporte">Esporte</option>
+                        <option value="street">Street</option>
+                        <option value="scooter">Scooter</option>
+                        <option value="custom">Custom</option>
+                    </select>
+                </p>
+                <p> Preço de custo: <input type="number" step="0.01" name="preco_custo" required> </p>
+                <p> Preço de venda: <input type="number" step="0.01" name="preco_venda" required> </p>
                 <p> Quantidade em estoque: <input type="number" name="quantidade" required> </p>
-                <p> Tipo de combustível: <input type="text" name="combustivel" required> </p>
+                <p> Tipo de combustível: 
+                    <select name="combustivel" required>
+                        <option value="gasolina">Gasolina</option>
+                        <option value="álcool">Álcool</option>
+                        <option value="flex">Flex</option>
+                    </select>
+                </p>
                 <p> Potência: <input type="text" name="potencia" required> </p>
                 <p> Sistema de freios: <input type="text" name="freios" required> </p>
-                <p> ABS: <input type="text" name="abs" required> </p>
+                <p> ABS: 
+                    <select name="abs" required>
+                        <option value="sim">Sim</option>
+                        <option value="não">Não</option>
+                    </select>
+                </p>
                 <p> Peso: <input type="text" name="peso" required> </p>
                 <p> Capacidade do tanque: <input type="text" name="tanque" required> </p>
-                <p> Tipo de partida: <input type="text" name="partida" required> </p>
-                <p> Status de disponibilidade: <input type="text" name="status" required> </p>
-                <p> Foto: <input type="file" name = "foto"> </p>
+                <p> Tipo de partida: 
+                    <select name="partida" required>
+                        <option value="elétrica">Elétrica</option>
+                        <option value="pedal">Pedal</option>
+                    </select>
+                </p>
+                <p> Status de disponibilidade: 
+                    <select name="status" required>
+                        <option value="disponível">Disponível</option>
+                        <option value="indisponível">Indisponível</option>
+                    </select>
+                </p>
+                <p> Foto: <input type="file" name="foto"> </p>
                 <p> <input type="submit" value="Cadastrar Moto"> </p>								
             </form>
-        </div>	
+        </div>    
         <div id="rodape">
             <div id="texto_institucional">
-                <div id="texto_institucional">
-                    <h6> CONTROLE DE MOTOS </h6> 
-                    <h6> Rua das Motos, 123 -- E-mail: contato@lojademotos.com.br -- Fone: (11) 9999-9999 </h6> 
-                </div> 
-            </div>
+                <h6> CONTROLE DE MOTOS </h6> 
+                <h6> Rua das Motos, 123 -- E-mail: contato@multimotos.com.br -- Fone: (11) 9999-9999 </h6> 
+            </div> 
         </div>
     </div>
 </body>
