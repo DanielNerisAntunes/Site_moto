db0055f30f9d9e17cec7e39118d80561577210d7
DanielNerisAntunes <danielnantunestecnicoetb@gmail.com>
2024-12-04 11:45:48 -0300

    Atualizar o processa_cadastra_fun.php
    upgrade

--- processa_cadastra_fun.php
+++ processa_cadastra_fun.php
@@ -1,63 +1,49 @@
 <?php
 // 1. Estabelecer uma conexão com o BD
-$conectar = mysqli_connect ("localhost", "root", "", "motos");
+$conectar = mysqli_connect("localhost", "root", "", "motos");
 
-// 2. Receber o nome, a função, login e senha	
+// 2. Receber os dados do formulário
 $nome = $_POST["nome"];
 $login = $_POST["login"];
 $senha = $_POST["senha"]; 
-// Aqui você deve usar password_hash() para criptografar a senha
-$senha_criptografada = password_hash($senha, PASSWORD_DEFAULT); 
+$senha_criptografada = password_hash($senha, PASSWORD_DEFAULT); // Criptografar a senha
 $funcao = $_POST["funcao"];
 
-// 3. Pesquisar no banco de dados se já existe o login que foi recebido acima
-$sql_consulta = "SELECT Login FROM funcionarios 
-                 WHERE Login = '$login'";
-                        
-$resultado_consulta = mysqli_query ($conectar, $sql_consulta);
-
-$linhas = mysqli_num_rows ($resultado_consulta);		
+// 3. Verificar se o login já existe no banco de dados
+$sql_consulta = "SELECT Login FROM funcionarios WHERE Login = ?";
+$stmt = mysqli_prepare($conectar, $sql_consulta);
+mysqli_stmt_bind_param($stmt, "s", $login); // Bind do parâmetro para o login
+mysqli_stmt_execute($stmt);
+mysqli_stmt_store_result($stmt);
+$linhas = mysqli_stmt_num_rows($stmt);
 
 if ($linhas == 1) {
-
     echo "<script> 
-                alert ('Login ja cadastrado. Tente outro!') 
+            alert('Login já cadastrado. Tente outro!'); 
           </script>";
-        
     echo "<script> 
-                location.href = ('cadastra_fun.php') 
-          </script>";			
-}
-else { // Para o usuario que não existe	
-    $sql_cadastrar = "INSERT INTO funcionarios 
-                                  (Nome, 
-                                  Funcao, 
-                                  Login, 
-                                  Senha) 
-                      VALUES 
-                                  ('$nome',
-                                  '$funcao',
-                                  '$login',
-                                  '$senha_criptografada')"; // Usar a senha criptografada aqui
-    $resultado_cadastrar = mysqli_query ($conectar, $sql_cadastrar);
+            location.href = 'cadastra_fun.php'; 
+          </script>";	
+} else {
+    // 4. Inserir o novo funcionário
+    $sql_cadastrar = "INSERT INTO funcionarios (Nome, Funcao, Login, Senha) VALUES (?, ?, ?, ?)";
+    $stmt_cadastrar = mysqli_prepare($conectar, $sql_cadastrar);
+    mysqli_stmt_bind_param($stmt_cadastrar, "ssss", $nome, $funcao, $login, $senha_criptografada);
     
-    if ($resultado_cadastrar == true) { 		
+    if (mysqli_stmt_execute($stmt_cadastrar)) {
         echo "<script> 
-                alert ('$nome cadastrado com sucesso') 
+                alert('$nome cadastrado com sucesso'); 
               </script>";
-        
         echo "<script> 
-                location.href = ('cadastra_fun.php') 
+                location.href = 'cadastra_fun.php'; 
+              </script>";	
+    } else {
+        echo "<script> 
+                alert('Ocorreu um erro no servidor. Tente de novo.'); 
+              </script>";
+        echo "<script> 
+                location.href = 'cadastra_fun.php'; 
               </script>";	
     }
-    else { 		
-        echo "<script> 
-                alert ('ocorreu um erro no servidor. Tente de novo') 
-              </script>";
-    
-        echo "<script> 
-                location.href = ('cadastra_fun.php') 
-              </script>";		
-    }	
 }
 ?>
